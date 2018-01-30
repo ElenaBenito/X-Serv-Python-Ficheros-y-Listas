65015703fabea32d25506333ed68dcdaf4421824
Elena Benito Del Pino <elenab@gamma01.aulas.gsyc.urjc.es>
2018-01-30 10:57:35 +0100

    He hecho la tabla de multiplicar

---
+++ multiplica.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+print("Tabla de multiplicar")
+
+for numero1 in range(1,11):
+	print("Tabla del ", numero1)
+	for numero2 in range(1,11):
+		print(numero1, " por ", numero2, " es ", numero1*numero2)
+
